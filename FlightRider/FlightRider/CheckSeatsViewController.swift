213f447bf61edbcefc113e9f88d8911f03fd4c69
Horvath <uia84397@contiwan.com>
2019-08-06 16:10:20 +0200

    Created almost the entire the check seat view programatically

--- FlightRider/FlightRider/CheckSeatsViewController.swift
+++ FlightRider/FlightRider/CheckSeatsViewController.swift
@@ -10,62 +10,111 @@ import UIKit
 
 class CheckSeatsViewController: UIViewController {
 
-    @IBOutlet weak var testView: UIView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var seatNumbersView: UIStackView!
+    @IBOutlet weak var ABC: UIStackView!
+    @IBOutlet weak var DEF: UIStackView!
+    @IBOutlet weak var test: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createSeatNumbers()
         drawRectangle()
-
+        //seatNumbersView.axis = NSLayoutConstraint.Axis.vertical
 
         // Do any additional setup after loading the view.
     }
     
+    func createSeatNumbers() {
+        
+        for i in 2...32{
+            let lbl = UILabel(frame: CGRect(x: 0, y: 50, width: 10, height: 10))
+            lbl.text = String(i)
+            lbl.font = UIFont(name: "Helvetica", size: 25)
+            seatNumbersView.addArrangedSubview(lbl)
+            
+            let stackViewABC = UIStackView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+            stackViewABC.axis = .horizontal
+            stackViewABC.distribution = .fill
+            stackViewABC.alignment = .fill
+            stackViewABC.spacing = 7
+            stackViewABC.translatesAutoresizingMaskIntoConstraints = false
+            
+            for _ in 0...2{
+                let seatview = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+                seatview.backgroundColor = .blue
+                seatview.layer.cornerRadius = 10
+                seatview.translatesAutoresizingMaskIntoConstraints = false
+                stackViewABC.addArrangedSubview(seatview)
+                NSLayoutConstraint(item: seatview, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
+                NSLayoutConstraint(item: seatview, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
+            }
+            contentView.addSubview(stackViewABC)
+            
+            NSLayoutConstraint(item: stackViewABC, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
+            NSLayoutConstraint(item: stackViewABC, attribute: .centerX, relatedBy: .equal, toItem: ABC, attribute: .centerX, multiplier: 1.0, constant: 0).isActive = true
+            NSLayoutConstraint(item: stackViewABC, attribute: .centerY, relatedBy: .equal, toItem: lbl, attribute: .centerY, multiplier: 1.0, constant: 0.0).isActive = true
+            
+            let stackViewDEF = UIStackView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+            stackViewDEF.axis = .horizontal
+            stackViewDEF.distribution = .fill
+            stackViewDEF.alignment = .fill
+            stackViewDEF.spacing = 7
+            stackViewDEF.translatesAutoresizingMaskIntoConstraints = false
+            
+            for _ in 0...2{
+                let seatview = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+                seatview.backgroundColor = .blue
+                seatview.layer.cornerRadius = 10
+                seatview.translatesAutoresizingMaskIntoConstraints = false
+                stackViewDEF.addArrangedSubview(seatview)
+                NSLayoutConstraint(item: seatview, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
+                NSLayoutConstraint(item: seatview, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
+            }
+            contentView.addSubview(stackViewDEF)
+            
+
+            
+            /*NSLayoutConstraint(item: stackViewABC, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 135).isActive = true*/
+            NSLayoutConstraint(item: stackViewDEF, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
+            NSLayoutConstraint(item: stackViewDEF, attribute: .centerX, relatedBy: .equal, toItem: DEF, attribute: .centerX, multiplier: 1.0, constant: 0).isActive = true
+            NSLayoutConstraint(item: stackViewDEF, attribute: .centerY, relatedBy: .equal, toItem: lbl, attribute: .centerY, multiplier: 1.0, constant: 0.0).isActive = true
+        }
+        print(contentView.frame.size)
+    }
+    
     func drawRectangle() {
-        /*let layer = CAShapeLayer()
-        layer.path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: 40, height: 40), cornerRadius: 5).cgPath
-        layer.fillColor = UIColor.red.cgColor
-        let subview = UIView()
-        subview.layer.addSublayer(layer)
-        subview.translatesAutoresizingMaskIntoConstraints = false
-        //subview.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
-        Tap.on(view: subview, fires: viewTapped)
-        view.addSubview(subview)
-        /*layer.path = UIBezierPath(roundedRect: CGRect(x: 64, y: 100, width: 40, height: 40), cornerRadius: 5).cgPath
-        layer.fillColor = UIColor.red.cgColor
-        let subview = UIView()
-        subview.layer.addSublayer(layer)
-        let gesture = UITapGestureRecognizer(target: self, action:  #selector (self.screenTapped(_:)))
-        subview.addGestureRecognizer(gesture)
-        view.addSubview(subview)*/
-        //subview.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
-        
-        NSLayoutConstraint(item: subview, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 64.0).isActive = true
-        NSLayoutConstraint(item: subview, attribute: .top, relatedBy: .equal, toItem: view, attribute: .topMargin, multiplier: 1.0, constant: 100.0).isActive = true
-        
-        //view.layer.addSubview(subview)*/
-        
-        
-        
-        
-        
-        let redView = UIView()
+     
+        /*let redView = UIView()
         redView.backgroundColor = .red
         redView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
         redView.layer.cornerRadius = 10.0
         redView.translatesAutoresizingMaskIntoConstraints = false
-        Tap.on(view: redView, fires: viewTapped)
-        view.addSubview(redView)
+        Tap.on(view: redView){
+            self.viewTapped(view: redView)
+        }
         
-        NSLayoutConstraint(item: redView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
+        let blackView = UIView()
+        blackView.backgroundColor = .black
+        blackView.frame = CGRect(x: 200, y: 850, width: 40, height: 40)
+        blackView.layer.cornerRadius = 10.0
+        blackView.translatesAutoresizingMaskIntoConstraints = false
+        Tap.on(view: blackView){
+            self.viewTapped(view: blackView)
+        }
+        
+        //view.addSubview(redView)
+        contentView.addSubview(blackView)
+        /*NSLayoutConstraint(item: redView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
         NSLayoutConstraint(item: redView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
         NSLayoutConstraint(item: redView, attribute: .leadingMargin, relatedBy: .equal, toItem: view, attribute: .leadingMargin, multiplier: 1.0, constant: 64.0).isActive = true
-        NSLayoutConstraint(item: redView, attribute: .topMargin, relatedBy: .equal, toItem: view, attribute: .topMargin, multiplier: 1.0, constant: 100.0).isActive = true
-        Tap.on(view: testView, fires: viewTapped)
-        
+        NSLayoutConstraint(item: redView, attribute: .topMargin, relatedBy: .equal, toItem: view, attribute: .topMargin, multiplier: 1.0, constant: 100.0).isActive = true*/
+        */
     }
     
-    func viewTapped(){
+    func viewTapped(view : UIView){
         print("Yay!")
+        view.backgroundColor = .blue
     }
     
     
